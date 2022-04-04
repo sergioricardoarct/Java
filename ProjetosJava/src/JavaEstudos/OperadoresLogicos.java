2434fcb81c30c9af2df9332ce101baa6b4c5f8cf
Sergio Ricardo <90733434+sergioricardoarct@users.noreply.github.com>
2022-04-04 19:36:14 -0300

    Add files via upload

---
+++ ProjetosJava/src/JavaEstudos/OperadoresLogicos.java
@@ -0,0 +1,13 @@
+package Fundamentos;
+
+public class OperadoresLogicos {
+	public static void main(String[] args) { 	
+		
+		double Nota = 7.7;
+		
+		String ResultadoFinal  = Nota >=7 ? "Aprovado.":"Recuperação.";			// Operação Lógica
+		
+			System.out.println("O aluno está:"+ ResultadoFinal);
+		
+	}
+}
